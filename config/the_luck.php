1fc4f3117b5d1ca4d2754ca839b8072138ee6765
Farhad Nowzari <fdn.sharp@gmail.com>
2020-05-07 00:58:18 +0200

    Sickness probability added

--- config/the_luck.php
+++ config/the_luck.php
@@ -2,5 +2,6 @@
 
 return [
     'contestants_per_contest' => env('CONTESTANTS_PER_CONTEST', 10),
-    'judges_per_round' => env('JUDGES_PER_ROUND', 3)
+    'judges_per_round' => env('JUDGES_PER_ROUND', 3),
+    'sickness_probability' => env('SICKNESS_PROBABILITY', 5)
 ];
