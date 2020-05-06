d36b82eb8e2b9f983562fd5f1c79f244450cec6c
Farhad Nowzari <fdn.sharp@gmail.com>
2020-05-06 23:24:18 +0200

    Initialized the view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+@section('content')
+    <div class="h-100 d-flex align-items-center">
+        <div class="container mw-md shadow-sm p-4 rounded bg-white">
+            
+        </div>
+    </div>
+@endsection
